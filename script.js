84e34a3b7383a92e689d837658e206025b5a29e8
Niraj Kumar <n1r4jkumar@gmail.com>
2022-04-13 05:56:03 +0530

    sample file has created

---
+++ script.js
@@ -0,0 +1,70 @@
+var banner = [
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1649683696/WebBanner/Banner323.png',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1649175874/WebBanner/Banner321.png',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1648873882/WebBanner/Banner318.png',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1648819057/WebBanner/Banner317.jpg',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1648819057/WebBanner/Banner317.jpg',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1644816631/WebBanner/Banner296.png',
+    'https://images.mcdelivery.co.in/hardcastle-restaurants-pvt-ltd/image/upload/f_auto,q_auto,w_420/v1646237005/WebBanner/Banner306.png',
+]
+
+var innerDIv = document.querySelector('.innerDIv');
+var waitOrder = document.querySelector('.waitOrder');
+var orderMessage = document.querySelector('#orderMessage');
+banner.map((e)=> {
+    var div = document.createElement('div');
+    div.innerHTML = `<img src="${e}" alt="">`;
+    innerDIv.append(div);
+})
+
+
+var itemOne = document.querySelector('#checkbox1');
+var itemTwo = document.querySelector('#checkbox2');
+var itemThree = document.querySelector('#checkbox3');
+var itemFour = document.querySelector('#checkbox4');
+
+async function orderPromis(time){
+    let myPromise = new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            resolve(`Order is Ready...`);
+        }, time*1000);
+        
+    });
+    orderMessage.innerHTML = await myPromise;
+}
+
+
+
+function run(time){
+    orderPromis(time+2);
+    var x = setInterval(function(){
+        if(time===0){
+            runStop();
+        }
+        waitOrder.innerHTML = `Wait for <span id='counter'>${time--} second</span>`
+        
+    },1000);
+
+    function runStop(){
+        clearInterval(x);
+        
+    }
+}
+
+function orderFood(){
+    // console.log(itemOne.checked)
+    // console.log(itemTwo.checked)
+    // console.log(itemThree.checked)
+    // console.log(itemFour.checked)
+    let time = Math.random()*8 +4;
+        console.log(time)
+        run(Math.floor(time));
+        
+        // orderFood(Math.floor(time));
+    // alert('order confirm');
+    itemOne.checked = false;
+    itemTwo.checked = false;
+    itemThree.checked = false;
+    itemFour.checked = false;
+}
+
